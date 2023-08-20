ecfd630bd784b5bd53d6a176ef371aaab7441495
Brayan <regimenbrian@gmail.com>
2023-08-20 11:36:37 -0500

    Agregando dependencias maven al proyecto

---
+++ src/com/alura/factory/ConnectionFactory.java
@@ -0,0 +1,26 @@
+package com.alura.factory;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+public class ConnectionFactory {
+	private DataSource datasource;
+	
+	public ConnectionFactory() {
+		var pooledDataSource = new ComboPooledDataSource();
+		pooledDataSource.setJdbcUrl("jdbc:mysql://localhost/hotel_alura?useTimeZone=true&serverTimeZone=UTC");
+		pooledDataSource.setUser("root");
+		pooledDataSource.setPassword("AirbenderX1980.");
+		pooledDataSource.setMaxPoolSize(10);
+	}
+	
+	public Connection recuperaConexion() {
+		try {
+			return this.datasource.getConnection();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
